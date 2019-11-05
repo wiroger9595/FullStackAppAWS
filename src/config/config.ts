1e30cc2c76af69df61a047bb9ab158935a804cf3
robert <robert@roberts-MacBook-Pro.local>
2019-11-04 17:28:43 -0800

    aa

---
+++ src/config/config.ts
@@ -0,0 +1,33 @@
+export const config = {
+  "dev": {
+    "username": "udacity",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "postgress":{
+    "username": "udacityrobertc",
+    "password": "udacityrobertc",
+    "database": "udacityrobertc",
+    "host": "udacityrobertc.chsx8vvvi8ic.us-east-2.rds.amazonaws.com",
+    "dialect": "postgress"    
+  },
+  "aws":{
+    "aws_reigion": "",
+    "aws_profile": "",
+    "aws_media_bucket": ""
+
+  }
+
+}
